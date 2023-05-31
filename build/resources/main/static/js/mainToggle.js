17d7fcad335d16ad379a87f983a0fc1c45601193
lhs-devloper <sdsd0114@naver.com>
2023-05-31 14:45:50 +0900

    merge manager/min

--- build/resources/main/static/js/mainToggle.js
+++ build/resources/main/static/js/mainToggle.js
@@ -4,6 +4,13 @@
 		let timer;
 		$(document).ready(function() {
 			$(".toggle--box--room").click(function() {
+				if($(".navi--bar--detail--fac").show() || 
+					$(".navi--bar--detail--reserve").show() ||
+					$(".navi--bar--detail--qna").show()) {
+						$(".navi--bar--detail--fac").hide();
+						$(".navi--bar--detail--reserve").hide();
+						$(".navi--bar--detail--qna").hide();
+					}
 				if(timer){
 					clearTimeout(timer)
 				}
@@ -15,6 +22,13 @@
 
 		$(document).ready(function() {
 			$(".toggle--box--fac").click(function() {
+				if($(".navi--bar--detail--room").show() || 
+					$(".navi--bar--detail--reserve").show() ||
+					$(".navi--bar--detail--qna").show()) {
+						$(".navi--bar--detail--room").hide();
+						$(".navi--bar--detail--reserve").hide();
+						$(".navi--bar--detail--qna").hide();
+					}
 				// javascript 쓰로틀링
 				if(timer){
 					clearTimeout(timer)
@@ -27,6 +41,13 @@
 		});
 		$(document).ready(function() {
 			$(".toggle--box--reserve").click(function() {
+				if($(".navi--bar--detail--room").show() || 
+					$(".navi--bar--detail--fac").show() ||
+					$(".navi--bar--detail--qna").show()) {
+						$(".navi--bar--detail--room").hide();
+						$(".navi--bar--detail--fac").hide();
+						$(".navi--bar--detail--qna").hide();
+					}
 				if(timer){
 					clearTimeout(timer)
 				}
@@ -37,6 +58,18 @@
 		});
 		$(document).ready(function() {
 			$(".toggle--box--qna").click(function() {
-				$(".navi--bar--detail--qna").slideToggle("slow");
+				if($(".navi--bar--detail--room").show() || 
+					$(".navi--bar--detail--fac").show() ||
+					$(".navi--bar--detail--reserve").show()) {
+						$(".navi--bar--detail--room").hide();
+						$(".navi--bar--detail--fac").hide();
+						$(".navi--bar--detail--reserve").hide();
+					}
+				if(timer){
+					clearTimeout(timer)
+				}
+				timer = setTimeout(function(){
+					$(".navi--bar--detail--qna").slideToggle("slow");
+				}, 200)
 			});
 		});
